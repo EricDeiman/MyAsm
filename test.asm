f7e4117faa735ec94004757848a78d4d23fbfbab
Eric <eric@system76-pc.localdomain>
2021-09-20 10:48:09 -0500

    add copyright and license

--- test.asm
+++ test.asm
@@ -1,3 +1,20 @@
+/*
+  The MyAsm programming language
+  Copyright 2019 Eric J. Deiman
+
+  This file is part of the MyAsm programming language.
+  The MyAsm programming language is free software: you can redistribute it
+  and/ormodify it under the terms of the GNU General Public License as published by the
+  Free Software Foundation, either version 3 of the License, or (at your option) any
+  later version.
+
+  The MyAsm programming language is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License along with the
+  MyAsm programming language. If not, see <https://www.gnu.org/licenses/>
+*/
+
 
 test.bin:     file format binary
 
